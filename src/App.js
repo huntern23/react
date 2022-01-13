ce43acb8b293f0fe31feeecb735787070206a1d6
Olexandr Strylbitskyi <lil.teern@gmail.com>
2022-01-13 14:10:57 +0200

    cw1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import "./App.css"
 const App = () => {
-  return (
+  let [flightsList,setFlightsList] = useState([]);
+    useEffect(()=>{
+      fetch("https://api.spacexdata.com/v3/launches/")
+          .then(value => value.json())
+          .then(flights => {
+            setFlightsList(flights.filter(flight=>flight.launch_year!=='2020'));
+          })
+  },[])
+    return (
       <div>
-
+          {
+              flightsList.map(value => <div className="block" key={value.flight_number}>
+                  <div><h2>{value.launch_year}</h2>
+                  <h1>{value.mission_name}</h1> </div>
+                  <img src={value.links.mission_patch} alt="mission_patch"/>
+              </div>)
+          }
       </div>
   );
 };
