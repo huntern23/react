59b92c681297cf6b9e7bd14b8d40db23f5dee6fa
Olexandr Strylbitskyi <lil.teern@gmail.com>
2022-02-06 17:58:21 +0200

    hw4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
 import "./App.css"
-const App = () => {
-  return (
-      <div>
+import {useReducer} from "react";
 
-      </div>
-  );
-};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return {...state, [`count${action.payload}`]: state[`count${action.payload}`] + 1}
+        case 'dec':
+            return {...state, [`count${action.payload}`]: state[`count${action.payload}`] - 1}
+        case 'reset':
+            return {...state, [`count${action.payload}`]: 0}
+        default:
+            throw new Error('ERROR')
+    }
+}
 
+function App() {
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
+    return (
+        <div>
+            <div>
+                <div>{state.count1}</div>
+                <button onClick={() => dispatch({type: 'inc', payload: 1})}>INC</button>
+                <button onClick={() => dispatch({type: 'dec', payload: 1})}>DEC</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 1})}>RESET</button>
+            </div>
+            <div>
+                <div>{state.count2}</div>
+                <button onClick={() => dispatch({type: 'inc', payload: 2})}>INC</button>
+                <button onClick={() => dispatch({type: 'dec', payload: 2})}>DEC</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 2})}>RESET</button>
+            </div>
+            <div>
+                <div>{state.count3}</div>
+                <button onClick={() => dispatch({type: 'inc', payload: 3})}>INC</button>
+                <button onClick={() => dispatch({type: 'dec', payload: 3})}>DEC</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 3})}>RESET</button>
+            </div>
+        </div>
+    );
+}
 export default App;
